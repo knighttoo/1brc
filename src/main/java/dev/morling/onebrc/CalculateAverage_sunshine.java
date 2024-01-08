4ed9a2e1021ab89992bdc38c868d2963cd3ec293
David Engel <david.engel@mercurygate.com>
2024-01-08 11:50:27 -0500

    Sunshine version 0.1

--- src/main/java/dev/morling/onebrc/CalculateAverage_sunshine.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_sunshine.java
@@ -15,11 +15,14 @@
  */
 package dev.morling.onebrc;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collector;
 
 import static java.util.stream.Collectors.groupingBy;
@@ -61,31 +64,63 @@ public class CalculateAverage_sunshine {
         // .collect(toMap(e -> e.getKey(), e -> Math.round(e.getValue() * 10.0) / 10.0)));
         // System.out.println(measurements1);
 
-        Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
-                MeasurementAggregator::new,
-                (a, m) -> {
-                    a.min = Math.min(a.min, m.value);
-                    a.max = Math.max(a.max, m.value);
-                    a.sum += m.value;
-                    a.count++;
-                },
-                (agg1, agg2) -> {
-                    var res = new MeasurementAggregator();
-                    res.min = Math.min(agg1.min, agg2.min);
-                    res.max = Math.max(agg1.max, agg2.max);
-                    res.sum = agg1.sum + agg2.sum;
-                    res.count = agg1.count + agg2.count;
+        // stream parallelism X
+        // stream buffer input X
+        // chuck out the lines
+        // move the work and remove news
 
-                    return res;
-                },
-                agg -> {
-                    return new ResultRow(agg.min, agg.sum / agg.count, agg.max);
+        Map<String, MeasurementAggregator> measrements = new ConcurrentHashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(Paths.get(FILE).toFile()))) {
+            reader.lines().parallel().forEach(line -> {
+                String[] parts = line.split(";");
+                Measurement measurement = new Measurement(parts);
+                measrements.compute(measurement.station(), (station, aggregator) -> {
+                    if (aggregator == null) {
+                        aggregator = new MeasurementAggregator();
+                    }
+                    aggregator.min = Math.min(aggregator.min, measurement.value);
+                    aggregator.max = Math.max(aggregator.max, measurement.value);
+                    aggregator.sum += measurement.value;
+                    aggregator.count++;
+                    return aggregator;
                 });
+            });
+        }
 
-        Map<String, ResultRow> measurements = new TreeMap<>(Files.lines(Paths.get(FILE))
-                .map(l -> new Measurement(l.split(";")))
-                .collect(groupingBy(m -> m.station(), collector)));
+        Map<String, String> resultsOf = new TreeMap<>();
+        for (Map.Entry<String, MeasurementAggregator> entryOf : measrements.entrySet()) {
+            MeasurementAggregator aggregator = entryOf.getValue();
+            resultsOf.put(entryOf.getKey(), new ResultRow(aggregator.min, aggregator.sum / aggregator.count, aggregator.max).toString());
+        }
+        System.out.println(resultsOf);
 
-        System.out.println(measurements);
+        /*
+         * Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
+         * MeasurementAggregator::new,
+         * (a, m) -> {
+         * a.min = Math.min(a.min, m.value);
+         * a.max = Math.max(a.max, m.value);
+         * a.sum += m.value;
+         * a.count++;
+         * },
+         * (agg1, agg2) -> {
+         * var res = new MeasurementAggregator();
+         * res.min = Math.min(agg1.min, agg2.min);
+         * res.max = Math.max(agg1.max, agg2.max);
+         * res.sum = agg1.sum + agg2.sum;
+         * res.count = agg1.count + agg2.count;
+         * 
+         * return res;
+         * },
+         * agg -> {
+         * return new ResultRow(agg.min, agg.sum / agg.count, agg.max);
+         * });
+         * 
+         * Map<String, ResultRow> measurements = new TreeMap<>(Files.lines(Paths.get(FILE))
+         * .map(l -> new Measurement(l.split(";")))
+         * .collect(groupingBy(m -> m.station(), collector)));
+         * 
+         * System.out.println(measurements);
+         */
     }
 }
